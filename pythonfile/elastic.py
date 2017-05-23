49c47cea284d3d02cc08a433fc5e1437c40aace6
Malte <j.malte-markwitz@t-online.de>
2017-05-23 03:07:04 +0200

    adjusting docker-compose pep8 and some return to show some stuff

--- pythonfile/elastic.py
+++ pythonfile/elastic.py
@@ -1,18 +1,22 @@
-from first import jsonstring
 import requests
 from elasticsearch import Elasticsearch
 
+from first import jsonstring
+
 es = Elasticsearch(['http://elasticsearch:9200'])
+
 
 def ping_elastic():
     res = requests.get('http://elasticsearch:9200')
     print("Elasticsearch ping: ", res.content)
 
-def store_json(idx, doctype, docbody={}):
+
+def store_json(idx, doctype, docbody):
     res = es.index(index=idx, doc_type=doctype, id=1, body=docbody)
     print(res)
     print()
-    
+
+
 def print_results(results, field):
     data = [doc for doc in results['hits']['hits']]
     for doc in data:
@@ -20,10 +24,12 @@ def print_results(results, field):
         print()
         print(doc)
 
+
 def elastic_example():
     ping_elastic()
-    store = store_json("test", "projects", jsonstring)
+    store_json("test", "projects", jsonstring)
     res = es.search(index="test", doc_type="projects", body={"query": {"match": {"status": "DONE"}}})
     print("jsonfile: %d documents found" % res['hits']['total'])
     print()
     print_results(res, 'status')
+    return "jsonfile: %d documents found" % res['hits']['total']
