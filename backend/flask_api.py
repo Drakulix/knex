e1954abb119bc2da43dac6c6019837f87b713942
DubbleClick <marc@henderkes.com>
2017-05-27 22:59:41 +0200

    merged methods into api/projects

--- backend/flask_api.py
+++ backend/flask_api.py
@@ -34,21 +34,56 @@ def index():
 
 # receive manifest as a jsonstring
 # returns new id
-@app.route('/api/projects', methods=['POST'])
+@app.route('/api/projects', methods=['GET', 'POST'])
 def add_project():
-    manifest=request.json
-    manifest['date_creation'] = time.strftime("%Y-%m-%d")
-    manifest['date_update'] = time.strftime("%Y-%m-%d")
+    if request.method == 'POST':
+        successful_files = []
+        unsuccessful_files = []
+        uploaded_files = request.files.getlist("file[]")
+        if len(uploaded_files) is not 0:
+            for file in uploaded_files:
+                securefilename = secure_filename(file.filename)
+                if file and uploader.allowed_file(securefilename):
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], securefilename))
+                    err = uploader.save_file_to_db(securefilename)
+                    if (err == None):
+                        successful_files.append(file.filename) #represent original filename
+                    else:
+                        unsuccessful_files.append(file.filename)
+                    print("Successful files: ", successful_files, '\n', file=sys.stderr)
+                    print("Unsuccessful files: ", unsuccessful_files, '\n', file=sys.stderr)
+            return      """<!doctype html>
+                        <title>Upload multiple files</title>
+                        <h1>Upload multiple files</h1>
+                        <body>Successful files: """ + ', '.join( e for e in successful_files) + '<br />' + """
+                        Unsuccessful files: """ + ', '.join( e for e in unsuccessful_files) + """
+                        </body>"""
 
-    error = validator.validate_manifest(manifest)
+        else: #no files attached
+            try:
+                err = None
+                if request.json:
+                    err = uploader.save_manifest_to_db(request.json)
+                else:
+                    err = uploader.save_manifest_to_db(json5.load(request.data))
 
-    if error == None:
-        manifest['id']=uuid.uuid4()
-        coll.insert(manifest)
-        elastic.store_json("test", "projects", manifest)
-        return make_response(manifest['id'])
-    else:
-        return make_response((error, '302'))
+                if err == None:
+                    return make_response("Successfully saved json to DB.")
+                else:
+                    return make_response("Exception while trying to save the json to DB.")
+            except Exception as error:
+                return make_response("error", '500')
+
+
+    elif request.method == 'GET': #remove this later, default multi file uploader for testing purposes
+        return """
+    <!doctype html>
+    <title>Upload multiple files</title>
+    <h1>Upload multiple files</h1>
+    <form action="" method=post enctype=multipart/form-data>
+    <input type=file name="file[]" multiple>
+    <input type=submit value=Upload>
+    </form>"""
 
 @app.route('/api/projects/<uuid:project_id>', methods=['GET'])
 def get_project_by_id(project_id):
@@ -67,65 +102,6 @@ def delete_project(project_id):
 def search():
     res=elastic.es.search(index="test", doc_type="projects", body=request.json)
     return jsonify(res)
-
-@app.route('/upload')
-def upload():
-    return render_template('upload.html')
-
-@app.route('/api/projects/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        securefilename = secure_filename(file.filename)
-        if file and uploader.allowed_file(securefilename):
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], securefilename))
-            if (uploader.save_file_to_db(securefilename) == None):
-                return render_template('upload_success.html')
-            else:
-                return render_template('upload_error.html')
-
-
-@app.route("/uploads", methods=["GET", "POST"])
-def uploads():
-    if request.method == 'POST':
-        successful_files = []
-        unsuccessful_files = []
-        uploaded_files = request.files.getlist("file[]")
-        for file in uploaded_files:
-            securefilename = secure_filename(file.filename)
-            if file and uploader.allowed_file(securefilename):
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], securefilename))
-                err = uploader.save_file_to_db(securefilename)
-                if (err == None):
-                    successful_files.append(file.filename) #represent original filename
-                else:
-                    unsuccessful_files.append(file.filename)
-                print("Successful files: ", successful_files, '\n', file=sys.stderr)
-                print("Unsuccessful files: ", unsuccessful_files, '\n', file=sys.stderr)
-        return """<!doctype html>
-    <title>Upload multiple files</title>
-    <h1>Upload multiple files</h1>
-    <body>Successful files: """ + ', '.join( e for e in successful_files) + '<br />' + """
-    Unsuccessful files: """ + ', '.join( e for e in unsuccessful_files) + """
-    </body>
-    """
-    
-    elif request.method == 'GET':
-        return """
-    <!doctype html>
-    <title>Upload multiple files</title>
-    <h1>Upload multiple files</h1>
-    <form action="" method=post enctype=multipart/form-data>
-    <input type=file name="file[]" multiple>
-    <input type=submit value=Upload>
-    </form>"""
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
