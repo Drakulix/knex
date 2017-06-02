da1a0a9d84cf3c1661560bbec468f5f0f15c64a5
JamesComey <maru1@zedat.fu-berlin.de>
2017-06-02 16:20:37 +0200

    New Api Calls for better search were added also the test function for dummy data is added

--- backend/flask_api.py
+++ backend/flask_api.py
@@ -165,5 +165,112 @@ def search():
         return (str(e), 400)
 
 
+
+# Search function which gets a text and does a full text search on all attributes
+@app.route('/api/projects/search/simple/<int:offset>/<int:count>/', methods=['GET'])
+def search_offset_count(offset, count):
+    text = request.args.get("q", type=str)
+    print(request.args.get("st", type=str),file=sys.stderr)
+#^3 is boosting the attribute, *_is allowing wildcards to be used
+    request_string = '{"from" : 1, "size" : 1, "query": {"multi_match" : {"query" : "" , "fields" : [ "tags^2", "title^2", "description" ] } } }'
+    request_json = json.loads(request_string)
+    request_json['query']['multi_match']['query'] = text
+    request_json['from'] = offset
+    request_json['size'] = count
+    print(json.dumps(request_json),file=sys.stderr)
+    try:
+        res = es.search(index="projects-index", doc_type="Project", body=request_json)
+        return jsonify(res['hits']['hits'])
+    except RequestError as e:
+        return (str(e), 400)
+
+
+#get projects containing having the search string somewhere within the tag field 
+@app.route('/api/projects/search/tag/<int:offset>/<int:count>/', methods=['GET'])
+def search_tag(offset, count):
+    tag = request.args.get('q', type=str)
+    request_string = '{"from" : 1, "size" : 1, "query": {"bool": {"must": [{"match_phrase": {"tags": ""} } ] } } }'
+    request_json = json.loads(request_string)
+    request_json['query']['bool']['must'][0]['match_phrase']['tags'] = tag
+    request_json['from'] = offset
+    request_json['size'] = count
+    try:
+        res = es.search(index="projects-index", doc_type="Project", body=request_json)
+        return jsonify(res['hits']['hits'])
+    except RequestError as e:
+        return (str(e), 400)
+
+
+@app.route('/api/projects/search/suggest/', methods=['GET'])
+def search_title():
+    text = request.args.get('q', type=str)
+    request_string = '{"suggest": {"text" : "","phraseSuggestion" : {"phrase":{"field" : "description","direct_generator":[{"field": "description","size": 10,"suggest_mode":"missing", "min_word_length": 3,"prefix_length":2}],"highlight": {"pre_tag": "<em>","post_tag": "</em>"} } } }}'
+    request_json = json.loads(request_string)
+    request_json['suggest']['text'] = text
+    try:
+        res = es.search(index="projects-index", doc_type="Project", body=request_json)
+        return (jsonify(res['suggest']['phraseSuggestion'][0]['options']))
+    except RequestError as e:
+        return (str(e), 400)
+
+# dummy add a few projects to es
+# Also sets up the mappings in elasticsearch which is necessary to do once in order to get suggestion
+#Note: deleting these projects will throw not found error bcause they are in es only, but they are deleted
+@app.route('/api/projects/dummyadd', methods=['GET'])
+def dummyadd():
+
+    settings = '{ "settings" : {"number_of_shards" : 1, "number_of_replicas" : 0}, "mappings" : {"suggest" : {"type" : "completion"},"title" : {"type": "completion"}, "description" : {"type": "string"}}}'
+    es.create(index="projects-index",doc_type='Project',id=0,body=settings)
+
+    output = "["
+
+    f = open('jsonprojectexamples/example0.json5', 'r')
+    doc =f.read()
+    res = es.index(index="projects-index", doc_type='Project', id=1, body=doc)
+    output += json.dumps(res) + " , "
+
+    f = open('jsonprojectexamples/example1.json5', 'r')
+    doc =f.read()
+    res = es.index(index="projects-index", doc_type='Project', id=2, body=doc)
+    output += json.dumps(res) + " , "
+
+    f = open('jsonprojectexamples/example2.json5', 'r')
+    doc =f.read()
+    res = es.index(index="projects-index", doc_type='Project', id=3, body=doc)
+    output += json.dumps(res) + " , "
+
+    f = open('jsonprojectexamples/example3.json5', 'r')
+    doc =f.read()
+    res = es.index(index="projects-index", doc_type='Project', id=4, body=doc)
+    output += json.dumps(res) + " , "
+
+    f = open('jsonprojectexamples/example4.json5', 'r')
+    doc =f.read()
+    res = es.index(index="projects-index", doc_type='Project', id=5, body=doc)
+    output += json.dumps(res) + " , "
+
+    f = open('jsonprojectexamples/example5.json5', 'r')
+    doc =f.read()
+    res = es.index(index="projects-index", doc_type='Project', id=6, body=doc)
+    output += json.dumps(res) + " , "
+
+    f = open('jsonprojectexamples/example6.json5', 'r')
+    doc =f.read()
+    res = es.index(index="projects-index", doc_type='Project', id=7, body=doc)
+    output += json.dumps(res) + " , "
+
+    f = open('jsonprojectexamples/example7.json5', 'r')
+    doc =f.read()
+    res = es.index(index="projects-index", doc_type='Project', id=8, body=doc)
+    output += json.dumps(res) + " , "
+
+    f = open('jsonprojectexamples/example8.json5', 'r')
+    doc =f.read()
+    res = es.index(index="projects-index", doc_type='Project', id=9, body=doc)
+    output += json.dumps(res) + " ]"
+
+    return (output)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
