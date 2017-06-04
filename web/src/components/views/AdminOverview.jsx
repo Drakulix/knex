456e808d74e3560cb6fee6bab8b7a50fe29ec711
Cedric Laier <Rintel@users.noreply.github.com>
2017-06-04 14:57:12 +0200

    Updated AdminOverview

--- web/src/components/views/AdminOverview.jsx
+++ web/src/components/views/AdminOverview.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-export default class UploadByLink extends React.Component {
+export default class AdminOverview extends React.Component {
   render() {
     return (
       <div className="container">
