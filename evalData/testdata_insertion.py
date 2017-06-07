05e1e1639c639ec7f9fab373c61ed75293a41fdc
Kristin Knorr <tini@unclouded.de>
2017-06-07 11:49:27 +0200

    added headers in request

--- evalData/testdata_insertion.py
+++ evalData/testdata_insertion.py
@@ -1,13 +1,21 @@
 import http.client
 import io
 import os
+import json
 
 if __name__ == "__main__":
     conn = http.client.HTTPConnection("0.0.0.0", 5000)
 
     for file in os.listdir("."):
         if file.endswith(".json"):
-            str = open(file, "r", encoding="utf-8").read()
-            conn.request("POST", "/api/projects", str.encode('utf-8'))
+            str = open(file, "r").read()
+            conn.request("POST", "/api/projects", str.encode('utf-8'),
+                        headers={'Content-Type': 'application/json'})
+            res = conn.getresponse()
+            print(res.status, res.reason)
+        elif file.endswith(".json5"):
+            str = open(file, "r").read()
+            conn.request("POST", "/api/projects", str.encode('utf-8'),
+                        headers={'Content-Type': 'application/json5'})
             res = conn.getresponse()
             print(res.status, res.reason)
