658cc5c2232bea47cef13d2711e69d014315e768
JamesComey <maru1@zedat.fu-berlin.de>
2017-06-08 23:59:55 +0200

    New search features and improvements

--- backend/flask_api.py
+++ backend/flask_api.py
@@ -199,5 +199,174 @@ def search():
         return (str(e), 400)
 
 
+@app.route('/api/projects/search/simple/', methods=['GET'])
+def search_simple():
+    """Search projects
+
+    Returns:
+        res (json): JSON containing Projects and metadata
+
+    """
+    text = request.args.get("q", type=str)
+    sorting = request.args.get('sort', type=str)
+    order = request.args.get('order', type=str)
+    offset = request.args.get('offset', type=int)
+    if offset is None:
+        offset = 0
+    count = request.args.get('count', type=int)
+    if count is None:
+        count = 10
+
+    # ^3 is boosting the attribute, *_is allowing wildcards to be used
+    request_json = {
+        'query': {
+            'multi_match': {
+                'query': text,
+                'fields': [
+                    'tags^2',
+                    'title^2',
+                    'description',
+                 ],
+            },
+        },
+        'from': offset,
+        'size': count,
+    }
+    if (sorting is not None) and (order is not None):
+        request_json["sort"] = dict()
+        request_json["sort"][sorting] = {
+            'order': order,
+        }
+
+    try:
+        res = es.search(index="projects-index", doc_type="Project", body=request_json)
+        return jsonify(res['hits'])
+    except RequestError as e:
+        return (str(e), 400)
+
+
+@app.route('/api/projects/search/advanced/', methods=['GET'])
+def search_avanced():
+    """Advanced search with filters
+
+    Returns:
+        res (json): JSON containing Projects and metadata
+
+    """
+    text = request.args.get("q", type=str)
+    sorting = request.args.get('sort', type=str)
+    order = request.args.get('order', type=str)
+    offset = request.args.get('offset', type=int)
+    if offset is None:
+        offset = 0
+    count = request.args.get('count', type=int)
+    if count is None:
+        count = 10
+
+    # ^3 is boosting the attribute, *_is allowing wildcards to be used
+    request_json = {
+        'query': {
+            'query_string': {
+                'query': text,
+            },
+        },
+        'from': offset,
+        'size': count,
+    }
+    if (sorting is not None) and (order is not None):
+        request_json["sort"] = dict()
+        request_json["sort"][sorting] = {
+            'order': order,
+        }
+    try:
+        res = es.search(index="projects-index", doc_type="Project", body=request_json)
+        return jsonify(res['hits'])
+    except RequestError as e:
+        return (str(e), 400)
+
+
+@app.route('/api/projects/search/tag/', methods=['GET'])
+def search_tag():
+    """Search projects
+
+    Returns:
+        res (json): JSON containing Projects and metadata
+    """
+    tag = request.args.get('q', type=str)
+    sorting = request.args.get('sort', type=str)
+    order = request.args.get('order', type=str)
+    offset = request.args.get('offset', type=int)
+    if offset is None:
+        offset = 0
+    count = request.args.get('count', type=int)
+    if count is None:
+        count = 10
+
+    request_json = {
+        'query': {
+            'bool': {
+                'must': [
+                    {
+                        'match_phrase': {
+                            'tags': tag,
+                        },
+                    },
+                ],
+            },
+        },
+        'from': offset,
+        'size': count,
+    }
+    if (sorting is not None) and (order is not None):
+        request_json["sort"] = dict()
+        request_json["sort"][sorting] = {
+            'order': order,
+        }
+    try:
+        res = es.search(index="projects-index", doc_type="Project", body=request_json)
+        return (jsonify(res['hits']))
+    except RequestError as e:
+        return (str(e), 400)
+
+
+@app.route('/api/projects/search/suggest/', methods=['GET'])
+def search_suggest():
+    """Suggests search improvements
+
+    Returns:
+        res (json): JSON containing suggested search terms
+    """
+    text = request.args.get('q', type=str)
+
+    request_json = {
+        'suggest': {
+            'text': text,
+            'phraseSuggestion': {
+                'phrase': {
+                    'field': "description",
+                    'highlight': {
+                        'pre_tag': '<em>',
+                        'post_tag': '</em>',
+                    },
+                    'direct_generator': [
+                        {
+                            'field': 'description',
+                            'size': 10,
+                            'suggest_mode': 'missing',
+                            'min_word_length': 3,
+                            'prefix_length': 2,
+                        }
+                    ],
+                },
+            },
+        },
+    }
+    try:
+        res = es.search(index="projects-index", doc_type="Project", body=request_json)
+        return (jsonify(res['suggest']['phraseSuggestion'][0]['options']))
+    except RequestError as e:
+        return (str(e), 400)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
