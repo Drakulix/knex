6f2fe0567c14ed3c53d7413d225a70a7685f4ddc
Cedric Laier <ced.laier@gmail.com>
2017-06-10 17:37:17 +0200

    integrated json form

--- web/src/components/views/CreateProjectByPattern.jsx
+++ web/src/components/views/CreateProjectByPattern.jsx
@@ -2,45 +2,69 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 import CreateProjectLink from '../pages/CreateProjectLink.jsx';
+import Form from "react-jsonschema-form";
 
+const schema = {
+  type: "object",
+  required: ["title"],
+  properties: {
+    title: {
+      type: "string",
+      title: "Title",
+      default: "A new task"
+    },
+    list: {
+      type: "array",
+      title: "A multiple choices list",
+      items: {
+        type: "string",
+        enum: ["foo", "bar", "fuzz", "qux"],
+      },
+      uniqueItems: true
+    },
+    ttztestst: {
+      type: "string", format: "uri"
+    },
+    tes: {
+      type: "string",
+      title: "Title",
+      default: "A new task"
+    },
+    bla: {
+      type: "array", title: "A multiple choices list",
+      items: {
+      type: "string",
+      enum: ["foo", "bar", "fuzz", "qux"]
+      },
+      uniqueItems: true
+    }
+  }
+};
+
+const uiSchema = {
+  "ui:widget": "checkboxes",
+  "ui:widget": "password",
+  "ui:help": "Hint: Make it strong!",
+  "ui:placeholder": "http://"
+};
+
+const log = (type) => console.log.bind(console, type);
 
 export default class UploadByPattern extends React.Component {
+
+  onSubmit() {
+  	alert('submitted!');
+  }
+
   render(){
     return(
       <div className="container">
         <div className="innerContainer">
           <div className="headerCreation">Create New Project</div>
-          <h3 className="caption">Project Name</h3>
-          <input className="enterValue" type="text"  ></input>
-          <h3 className="caption">Author Name</h3>
-          <input className="enterValue" type="text"  ></input>
-          <h3 className="caption">Team</h3>
-          <select className="dropdown" name="Select Team" size="1">
-            <option></option>
-            <option></option>
-            <option></option>
-            <option></option>
-            <option></option>
-          </select>
-          <h3 className="caption">Status</h3>
-          <select className="dropdown " name="Select Team" size="1">
-            <option>finished</option>
-            <option>pending</option>
-            <option>reviewed</option>
-          </select>
-          <h3 className="caption">Description</h3>
-          <textarea className="enterDescription" type="text"  ></textarea>
-          <h3 className="caption">Tags</h3>
-          <input className="tags" type="text">
-            {//To be filled
-            }
-          </input>
-          <div className="buttons">
-            <button className="saveButton" >Save</button>
-              <Link to="/createbylink">
-                <button className="cancelButton">Cancel</button>
-              </Link>
-          </div>
+            <Form schema={schema}
+              onChange={log("changed")}
+              onSubmit={this.onSubmit}
+              onError={log("errors")} />
         </div>
       </div>
   )
