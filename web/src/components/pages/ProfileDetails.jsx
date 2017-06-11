8d4941d5f4062d76484270593722ef89835e6b5e
Cedric Laier <ced.laier@gmail.com>
2017-06-11 19:06:05 +0200

    init profile view

---
+++ web/src/components/pages/ProfileDetails.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import ProfileContainer from '../views/ProfileContainer.jsx';
+import TopBar from '../common/TopBar';
+import { Link } from 'react-router-dom';
+
+export default class ProfileContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menu: {
+        discoverProjects: 'Discover Projects',
+        createProject: 'Create New Project',
+        adminArea: 'Admin Area'
+      }
+    };
+  }
+
+  render() {
+    return (
+      <div className="inner-content">
+        <TopBar />
+        <div className="row">
+          <div className="col-2 side-bar">
+            <ul className="list-group">
+              <li className="list-group-item">
+                <Link to="/discovery">
+                  {this.state.menu.discoverProjects}
+                </Link>
+              </li>
+              <li className="list-group-item">
+                <Link to="/createbylink">
+                  {this.state.menu.createProject}
+                </Link>
+              </li>
+              <li className="list-group-item">
+                <Link to="/admin">
+                  {this.state.menu.adminArea}
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="col-10">
+            <ProfileContainer />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ web/src/components/views/ProfileContainer.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class ProfileContainer extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="header">Profile Details</div>
+      </div>
+    );
+  }
+}
