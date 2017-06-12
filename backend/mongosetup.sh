03f5b956dca41620b2f3825094e8336794afd8b0
tschiersch <tschiersch@mi.fu-berlin.de>
2017-06-12 15:14:33 +0200

    tried replicating mongodb with only one replica, not working so far

--- backend/mongosetup.sh
+++ backend/mongosetup.sh
@@ -3,7 +3,13 @@
 mongod --replSet rs0
 
 mongo <<EOF
-	rs.initiate()
-	rs.conf()
-	rs.status()
-EOF
+	rs.initiate( {
+		"_id": 'rs0',
+		"members": [
+			{
+				"_id": 1,
+				"host": "mongodb:27017"
+			}
+		]
+	})
+EOF>>
