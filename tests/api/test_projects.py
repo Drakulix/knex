dde79cb55e077cdc5f5ec0560fb8087e34795778
Malte <j.malte-markwitz@t-online.de>
2017-06-15 23:59:48 +0200

    added test for database consistency and post consistency
    this checks if th data is the same for  es and mongo

--- tests/api/test_projects.py
+++ tests/api/test_projects.py
@@ -1,5 +1,6 @@
 import json
 import os
+import time
 from uuid import UUID
 
 import requests
@@ -59,6 +60,37 @@ class TestPOST(object):
     def test_missformed_content_type(self, flask_api_url):
         assert True
 
+    def test_inconsistent_post(self, flask_api_url, pytestconfig, mongo_client, elastic_client):
+        test_manifest = os.path.join(
+            str(pytestconfig.rootdir),
+            'tests',
+            'testmanifests',
+            'validexample0.json5'
+        )
+        with open(test_manifest, 'r', encoding='utf-8') as tf:
+            data = str(tf.read().replace('\n', ''))
+        response = requests.post(flask_api_url + "/api/projects", data=data.encode('utf-8'),
+                                 headers={'Content-Type': 'application/json5'})
+        print(response.text)
+        for project_id in response.json():
+            # This should pass if post is working properly
+            assert UUID(project_id, version=4)
+            project_uuid = UUID(project_id, version=4)
+            time.sleep(5)
+            print(project_id)
+            mongo_test = mongo_client.projects.find({})
+            mongo_result = mongo_client.projects.find_one(project_uuid)
+            print("mongo:", mongo_result)
+
+            # fails for mongo error
+            assert mongo_result["_id"] == project_uuid
+            es_result = elastic_client.get(index="knexdb", id=project_id)
+            print("es:", es_result)
+            # fails for es not found error
+            assert es_result['found']
+        # Start checking both databases
+
+
 
 class TestDELETE(object):
     def test_unknown_id(self, flask_api_url):
@@ -67,7 +99,7 @@ class TestDELETE(object):
     def test_success(self, flask_api_url):
         assert True
 
-    def test_unconsistent_delete(self, flask_api_url):
+    def test_inconsistent_delete(self, flask_api_url):
         assert True
 
 
