d7c903e4f2e649c2f620ce18ee12309666b1b81c
DubbleClick <marc@henderkes.com>
2017-06-19 12:52:00 +0200

    hopefully correct now

--- backend/mongodb_replica_setup/setup.sh
+++ backend/mongodb_replica_setup/setup.sh
@@ -1,18 +1,18 @@
-MONGODB=`ping -c 1 mongodb_replica | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
+# MONGODB=`ping -c 1 mongodb_replica | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 
 echo "Waiting for startup.."
 mongo --nodb wait.js
 echo "Started.."
 
 echo SETUP.sh time now: `date +"%T" `
-mongo --host ${MONGODB}:27017 <<EOF
+mongo --host mongodb:27017 <<EOF
    var cfg = {
         "_id": "projectsRepSet",
 
         "members": [
             {
                 "_id": 0,
-                "host": "${MONGODB}:27017",
+                "host": "mongodb:27017",
             },
 			{
 			    "_id": 1,
