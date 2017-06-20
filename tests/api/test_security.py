ba0d2653ba3c1b6744fc556ebec0c68d18fbb339
aTepliak <a.tepliak@gmail.com>
2017-06-20 21:30:44 +0200

    pep8 line length bug

--- tests/api/test_security.py
+++ tests/api/test_security.py
@@ -3,43 +3,43 @@ import unittest
 import os
 
 
-
 class TestSecurity(object):
-
     def setUp(self):
         # app['TESTING'] = True
         # self.app = app.test_client()
         pass
 
-    def test_main_page(self,flask_api_url):
+    def test_main_page(self, flask_api_url):
         response = requests.get(flask_api_url + '/')
-        assert  response.status_code == 404
+        assert response.status_code == 404
 
-    def test_login_fake_user(self,flask_api_url):
-        response = requests.post(flask_api_url + '/api/users/login', data=dict(email='user1', password='password'))
+    def test_login_fake_user(self, flask_api_url):
+        response = requests.post(flask_api_url + '/api/users/login',
+                                 data=dict(email='user1', password='password'))
         print(response)
         assert response.reason == 'INTERNAL SERVER ERROR'
         assert response.status_code == 500
 
-    def test_login_real_user(self,flask_api_url):
+    def test_login_real_user(self, flask_api_url):
         response = requests.post(flask_api_url + '/api/users/login',
                                  data=dict(email='admin@knex.com', password="admin"))
         assert response.reason == 'OK'
         assert response.status_code == 200
 
-    def test_login_real_user_wrong_psswd(self,flask_api_url):
-        response = requests.post(flask_api_url + '/api/users/login', data=dict(email='admin', password='a'))
-        assert response.reason =='INTERNAL SERVER ERROR'
+    def test_login_real_user_wrong_psswd(self, flask_api_url):
+        response = requests.post(flask_api_url + '/api/users/login',
+                                 data=dict(email='admin', password='a'))
+        assert response.reason == 'INTERNAL SERVER ERROR'
         assert response.status_code == 500
 
-    def test_logout(self,flask_api_url):
+    def test_logout(self, flask_api_url):
         response = requests.get(flask_api_url + '/api/users/login',
-                                 data=dict(email='user@knex.com', password="user"))
+                                data=dict(email='user@knex.com', password="user"))
         response = requests.get(flask_api_url + '/api/users/logout')
         assert response.reason == 'OK'
         assert response.status_code == 200
 
-    def test_access_login_requiered_logged(self, pytestconfig,flask_api_url):
+    def test_access_login_requiered_logged(self, pytestconfig, flask_api_url):
         test_manifest = os.path.join(
             str(pytestconfig.rootdir),
             'tests',
@@ -53,7 +53,7 @@ class TestSecurity(object):
 
         assert response.status_code == 200
 
-    def test_access_login_requiered_not_logged(self, pytestconfig,flask_api_url):
+    def test_access_login_requiered_not_logged(self, pytestconfig, flask_api_url):
         test_manifest = os.path.join(
             str(pytestconfig.rootdir),
             'tests',
@@ -65,11 +65,11 @@ class TestSecurity(object):
         response = requests.post(flask_api_url + "/api/projects", data=data.encode('utf-8'),
                                  headers={'Content-Type': 'application/json5'})
 
-        assert response.status_code == 200 #or 500?
+        assert response.status_code == 200  # or 500?
 
     def test_update_user(self, flask_api_url):
         response = requests.get(flask_api_url + '/api/users/', data=dict(email='user@knex.com'))
-        assert response.status_code == 405 #200?
+        assert response.status_code == 405  # 200?
 
     def test_update_user_not_exists(self, flask_api_url):
         response = requests.get(flask_api_url + '/api/users/', data=dict(email='unknownuser@knex.com'))
