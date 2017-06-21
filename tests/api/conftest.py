1b7298be626fd4130aa04012904c5de0dd526703
markwitz <j.m.markwitz@fu-berlin.de>
2017-06-21 11:24:09 +0200

    added fixture that drops the projects collection, added fixture add project

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -1,8 +1,13 @@
+import json
+import os
+from os.path import abspath, dirname
+
 import docker
 import pytest
 import requests
 
 from elasticsearch import Elasticsearch
+from jsonschema import Draft4Validator, FormatChecker
 from pymongo import MongoClient
 
 
@@ -43,3 +48,42 @@ def session():
                             data=data)
     yield session
     response = session.get(flask_api_url() + '/api/users/logout')
+
+
+@pytest.fixture(scope='session')
+def manifest_validator():
+    path_to_schema = os.path.join(abspath(dirname(dirname(__file__))), os.path.pardir,
+                                  "backend", "manifest_schema.json")
+    with open(path_to_schema, mode='r') as schema_file:
+        schema = json.load(schema_file)
+    validator = Draft4Validator(schema, format_checker=FormatChecker())
+    return validator
+
+
+@pytest.yield_fixture(autouse=True)
+def run_around_tests(mongo_client):
+    # Code that will run before your test, for example:
+    # files_before = # ... do something to check the existing files
+    result = mongo_client.projects.delete_many({})
+    # A test function will be run at this point
+    yield
+    # Code that will run after your test, for example:
+    result = mongo_client.projects.delete_many({})
+    # files_after = # ... do something to check the existing files
+    # assert files_before == files_after
+
+
+@pytest.fixture(scope="session")
+def enter_data_using_post(pytestconfig, flask_api_url):
+    test_manifest = os.path.join(
+        str(pytestconfig.rootdir),
+        'tests',
+        'testmanifests',
+        'validexample0.json5'
+    )
+    with open(test_manifest, 'r', encoding='utf-8') as tf:
+        data = str(tf.read().replace('\n', ''))
+    response = requests.post(flask_api_url + "/api/projects", data=data.encode('utf-8'),
+                             headers={'Content-Type': 'application/json5'})
+    print(response.text)
+    return response
