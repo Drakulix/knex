b5f71beeb8515338aaa4fe43ad1843379bed67c0
pdler <oliver.hanssen@gmail.com>
2017-06-29 17:37:01 +0200

    [User API] Bookmark tests: pep8

--- tests/api/test_bookmarks.py
+++ tests/api/test_bookmarks.py
@@ -2,42 +2,40 @@ import requests
 from flask import jsonify
 import uuid
 
+
 class TestBookmarks(object):
 
     def get_Bookmarks_without_logging(self, flask_api_url):
-
         response = requests.get(flask_api_url + "/api/users/bookmarks")
-
         assert response.status_code == 400
 
-
     def get_empty_bookmark(self, flask_api_url):
-        payload = {'email' : 'admin@knex.com', 'password' : "admin"}
-        response = requests.post(flask_api_url + "/api/users/login", data=payload)
+        payload = {'email': 'admin@knex.com', 'password': "admin"}
+        response = requests.post(flask_api_url + "/api/users/login",
+                                 data=payload)
         response = requests.get(flask_api_url + "/api/users/bookmarks")
 
         assert jsonify([]) == response.text
-
 
     def insert_project_to_bookmark(self, flask_api_url):
         local_list = []
         id1 = uuid.uuid4()
         local_list.append(id1)
-        payload = {'email' : 'admin@knex.com', 'password' : "admin"}
-        response = requests.post(flask_api_url + "/api/users/bookmarks/" + str(id1))
+        payload = {'email': 'admin@knex.com', 'password': "admin"}
+        response = requests.post(flask_api_url + "/api/users/bookmarks/" +
+                                 str(id1))
 
         assert jsonify(local_list) == response.text
-
 
     def delete_project_from_bookmark(self, flask_api_url):
         local_list = []
         id1 = uuid.uuid4()
         local_list.append(id1)
-        payload = {'email' : 'admin@knex.com', 'password' : "admin"}
-        response = requests.remove(flask_api_url + "/api/users/bookmarks/" + str(id1))
+        payload = {'email': 'admin@knex.com', 'password': "admin"}
+        response = requests.remove(flask_api_url + "/api/users/bookmarks/" +
+                                   str(id1))
         local_list.remove(id1)
         assert jsonify(local_list) == response.text
-
 
     def insert_multiple_project_to_bookmark(self, flask_api_url):
         local_list = []
@@ -53,9 +51,9 @@ class TestBookmarks(object):
         requests.post(flask_api_url + "/api/users/bookmarks/" + str(id1))
         requests.post(flask_api_url + "/api/users/bookmarks/" + str(id2))
         requests.post(flask_api_url + "/api/users/bookmarks/" + str(id3))
-        response = requests.post(flask_api_url + "/api/users/bookmarks/" + str(id4))
+        response = requests.post(flask_api_url + "/api/users/bookmarks/" +
+                                 str(id4))
         assert jsonify(local_list) == response.text
-
 
     def insert_multiple_project_to_bookmark(self, flask_api_url):
         local_list = []
@@ -72,6 +70,7 @@ class TestBookmarks(object):
         requests.post(flask_api_url + "/api/users/bookmarks/" + str(id2))
         requests.post(flask_api_url + "/api/users/bookmarks/" + str(id3))
         requests.post(flask_api_url + "/api/users/bookmarks/" + str(id4))
-        response = requests.delete(flask_api_url + "/api/users/bookmarks/" + str(id3))
+        response = requests.delete(flask_api_url + "/api/users/bookmarks/" +
+                                   str(id3))
         local_list.remove(id3)
         assert jsonify(local_list) == response.text
