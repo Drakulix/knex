ecaf5a19f293d1db052f6ad9add35f9d789e95a8
phil-schmidt <phil.schmidt@gmx.de>
2017-07-05 04:13:57 +0200

    Implemented basic error handling for create by link
    -Create by link will no longer redirect to crete by form if the provided link does not contain a valid json file and will display an error message.

--- web/src/components/views/CreateProjectView.jsx
+++ web/src/components/views/CreateProjectView.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router'
 import history from '../common/history'
 import 'isomorphic-fetch';
 
@@ -9,14 +10,43 @@ export default class UploadByLink extends React.Component {
     super(props);
     this.state = {
      sourceURL : "",
+     redirect : false,
     };
+    this.submitForm = this.submitForm.bind(this);
   }
 
+  submitForm(e){
+      e.preventDefault();
+      var request = new Request(this.state.sourceURL,{
+        method:'GET',
+        mode: 'cors',
+        header:{
+        'Access-Control-Allow-Origin':'*'
+        },
+      });
+      var that = this;
+      fetch(request)
+      .then(response => response.json())
+      .then(
+        function(){
+          that.setState({
+            redirect : true,
+          });
+        },
+        function(exception){
+          alert("Could not read file.\nParser returned:\n"+exception);
+        }
+      );
+  };
+
   render() {
+    if (this.state.redirect){
+      return <Redirect to={'/create/'+encodeURIComponent(this.state.sourceURL)}/>;
+    }
     return (
       <div className="container">
         <div className="header">Create Project from Manifest</div>
-        <form onSubmit={() => {history.push("/create/"+encodeURIComponent(this.state.sourceURL))}}>
+        <form onSubmit={this.submitForm}>
           <input className="enterUrl"
                  id="url"
                  type="text"
@@ -26,6 +56,7 @@ export default class UploadByLink extends React.Component {
                  })}
                  value={this.state.sourceURL}></input>
         </form>
+        <h1> {this.state.jsonIsValid}</h1>
         <div className="text">or</div>
         <Link to="/create">
           <button className="submit">Create New Project</button>
