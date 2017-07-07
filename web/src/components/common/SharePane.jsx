f703b6eeedd62ea7a35cd2d1e0cdcee91b3804df
harasic <harasic@inf.fu-berlin.de>
2017-07-07 13:13:35 +0200

    Refactor

--- web/src/components/common/SharePane.jsx
+++ web/src/components/common/SharePane.jsx
@@ -5,7 +5,7 @@ import ChipInput from 'material-ui-chip-input';
 import Chip from 'material-ui/Chip';
 import AutoComplete from 'material-ui/AutoComplete'
 import styles from '../common/Styles.jsx'
-
+import ChipInputList from '../common/ChipInputList.jsx'
 
 
 
@@ -16,6 +16,8 @@ export default class SharePane extends React.Component {
       open: false,
       authors:[]
     };
+
+    this.handleAuthorChange = this.handleAuthorChange.bind(this);
   }
 
   handleOpen = () => {
@@ -39,7 +41,7 @@ export default class SharePane extends React.Component {
     var suggestedAuthors = [{id:"marko@knex.", name :"Marko"},
     {id:"victor@knex", name :"Victor"},{id:"cedric@knex", name :"Cedric"}];
 
-//loadSuggestedAuthors
+    //loadSuggestedAuthors
 
     var suggestedAuthorsArray = []
     for (var i in suggestedAuthors) {
@@ -48,21 +50,11 @@ export default class SharePane extends React.Component {
     this.setState({suggestedAuthors: suggestedAuthorsArray});
   }
 
-  handleRequestAdd (chip, name) {
-    if(this.state["suggestedAuthors"].indexOf(chip) === -1)
-        return;
-     this.setState({
-       [name]: [...this.state[name], chip]
-     })
-   }
 
-  handleRequestDelete (deletedChip, name) {
-     this.setState({
-         [name]: this.state[name].filter((c) => c !== deletedChip)
-       })
+  handleAuthorChange(value) {
+    this.setState({authors : value});
+    this.props.onChange("tags" , value);
   }
-
-
 
   render() {
     const actions = [
@@ -70,42 +62,31 @@ export default class SharePane extends React.Component {
         label="Cancel"
         primary={true}
         onTouchTap={this.handleClose}
-      />,
+        />,
       <RaisedButton
         label="Share"
         primary={true}
         onTouchTap={this.handleClose}
         style={{marginLeft:20}}
-      />,
+        />,
     ];
 
     return (
-        <Dialog
-          title="Share project with"
-          actions={actions}
-          modal={false}
-          open={this.state.open}
-          onRequestClose={this.handleClose}
+      <Dialog
+        title="Share project with"
+        actions={actions}
+        modal={false}
+        open={this.state.open}
+        onRequestClose={this.handleClose}
         >
-          <ChipInput
-                 dataSource={this.state.suggestedAuthors}
-                 value={this.state.authors}
-                 filter={AutoComplete.fuzzyFilter}
-                 hintText='add users...'
-                 onRequestAdd={(chip) => this.handleRequestAdd(chip, "authors")}
-                 onRequestDelete={(deletedChip) => this.handleRequestDelete(deletedChip, "authors")}
-                 fullWidth
-                 chipRenderer={({ value, isFocused, isDisabled, handleClick, handleRequestDelete }, key) => (
-                   <Chip
-                           key={key}
-                           style= {styles["chip"]}
-                           backgroundColor={styles.chip.background}
-                           onTouchTap={handleClick}
-                           onRequestDelete={handleRequestDelete}>
-                          <span style={styles["chipText"]}> {value} </span>
-                   </Chip>
-                )}/>
-        </Dialog>
+        <ChipInputList suggestions = {this.state.suggestedAuthors}
+          onChange={this.handleAuthorChange}
+          filtered ={true}
+          value={this.state.authors}
+          hintText={'Add authors...'}
+          />
+
+      </Dialog>
     );
   }
 }
