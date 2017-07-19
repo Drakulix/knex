e8a7186ad36d595632a88264ef143fd2fb5f3857
gitmirgut <gitmirgut@users.noreply.github.com>
2017-07-19 19:35:02 +0200

    Add comment badge.

--- web/src/components/common/CommentSideBar.jsx
+++ web/src/components/common/CommentSideBar.jsx
@@ -52,6 +52,7 @@ export default class CommentSideBar extends React.Component {
       comment : ""
     })
     this.loadComments()
+    this.props.callback()
   }
 
   handleToggle = () => this.setState({showCommentBar : !this.state.showCommentBar})
