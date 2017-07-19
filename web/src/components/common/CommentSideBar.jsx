d28903c33417d3c75c85ed842f009b5030eb9a7a
gitmirgut <gitmirgut@users.noreply.github.com>
2017-07-19 22:36:36 +0200

    Add functionality for deleting comments.

--- web/src/components/common/CommentSideBar.jsx
+++ web/src/components/common/CommentSideBar.jsx
@@ -3,8 +3,54 @@ import Drawer from 'material-ui/Drawer'
 import RaisedButton from 'material-ui/RaisedButton'
 import {List, ListItem} from 'material-ui/List'
 import Divider from 'material-ui/Divider'
-import {get} from './Backend'
+import {get, del} from './Backend'
+import {grey400, darkBlack, lightBlack} from 'material-ui/styles/colors';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import TextField from 'material-ui/TextField'
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+
+
+class CommentItem extends React.Component {
+  constructor(){
+    super();
+  }
+
+  handleDelete(event){
+    var fetchURL ="/api/projects/"+this.props.p_id+"/comment/"+this.props.comment.id
+    del(fetchURL)
+    this.props.handleDeleted()
+    }
+
+  render(){
+    const iconButtonElement = (
+      <IconButton
+        touch={true}
+        tooltip="more"
+        tooltipPosition="bottom-left"
+      >
+        <MoreVertIcon color={grey400} />
+      </IconButton>
+    );
+
+    const rightIconMenu = (
+      <IconMenu iconButtonElement={iconButtonElement}>
+        <MenuItem onClick = {()=>console.log('Edit')}>Edit</MenuItem>
+        <MenuItem onClick = {()=>this.handleDelete()} >Delete</MenuItem>
+      </IconMenu>
+    );
+    return (
+    <ListItem primaryText={this.props.comment.message}
+              secondaryText={<div>
+                <span style={{float : "left"}}>{this.props.comment.author.email}</span>
+                <span style={{float : "right"}}> {this.props.comment.datetime}</span>
+              </div>}
+              rightIconButton={rightIconMenu}
+    />
+  )
+  }
+}
 
 export default class CommentSideBar extends React.Component {
 
@@ -15,6 +61,7 @@ export default class CommentSideBar extends React.Component {
     comments : []}
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleDeleted = this.handleDeleted.bind(this)
   }
 
   transformArray(dataArray) {
@@ -30,6 +77,11 @@ export default class CommentSideBar extends React.Component {
     this.setState({
       comment : value
     })
+  }
+
+  handleDeleted(event){
+    this.loadComments()
+    this.props.callback()
   }
 
   handleSubmit(event){
@@ -69,7 +121,6 @@ export default class CommentSideBar extends React.Component {
     var fetchURL = "/api/projects/" + this.props.uuid + "/comment"
     get(fetchURL).then(function(data) {
       var filteredData = this.transformArray(data)
-      console.log(data)
       this.setState({
         comments : filteredData
       })
@@ -101,9 +152,7 @@ export default class CommentSideBar extends React.Component {
         {this.state.comments.map(item =>
           <div>
             <Divider/>
-            <ListItem primaryText={item.message}
-                      secondaryText={<div><span style={{float : "left"}}>{item.author.email}</span><span style={{float : "right"}}> {item.datetime}</span></div>}
-            />
+            <CommentItem comment={item} p_id={this.props.uuid} handleDeleted={this.handleDeleted}/>
           </div>)
         }
       </List>
