6af9d8df7db262f42bd232551fc4055d73870573
aTepliak <a.tepliak@gmail.com>
2017-07-20 23:48:16 +0200

    class added

--- tests/api/test_notifications.py
+++ tests/api/test_notifications.py
@@ -6,36 +6,32 @@ import requests
 
 from uuid import UUID
 
-projects_ids = []
-comment_id = ""
 
-
-def set_up(self, flask_api_url, pytestconfig):
-    # login first
-    requests.post(flask_api_url + '/api/users/login',
-                  data=dict(email='admin@knex.com', password="admin"))
-    # add some project and save id
-    test_manifest = os.path.join(
-        str(pytestconfig.rootdir),
-        'tests',
-        'testmanifests',
-        'validexample1.json5'
-    )
-    with open(test_manifest, 'r', encoding='utf-8') as tf:
-        data = str(tf.read().replace('\n', ''))
-    response = requests.post(flask_api_url + "/api/projects", data=data.encode('utf-8'),
-                            headers={'Content-Type': 'application/json5'})
-    print(response.text)
-    for pid in response.json():
-        projects_ids.append(pid)
-
-
-def test_project_notification(self, flask_api_url):
-    requests.post(
-        flask_api_url + '/api/projects/' + projects_ids[0] + '/comment',
-        data='new comment')
-    requests.get(flask_api_url + '/api/users/logout')
-    requests.post(flask_api_url + '/api/users/login',
-                  data=dict(email='user@knex.com', password="user"))
-    user_notifications = requests.get(flask_api_url + '/api/users/notifications')
-    assert (user_notifications['link'] == flask_api_url + '/project/' + projects_ids[0])
+class Test_notifications(object):
+    def test_project_notification(self,session, flask_api_url, pytestconfig):
+        # login first
+        requests.post(flask_api_url + '/api/users/login',
+                      data=dict(email='admin@knex.com', password="admin"))
+        # add some project and save id
+        test_manifest = os.path.join(
+            str(pytestconfig.rootdir),
+            'tests',
+            'testmanifests',
+            'validexample01.json5'
+        )
+        with open(test_manifest, 'r', encoding='utf-8') as tf:
+            data = str(tf.read().replace('\n', ''))
+        response = session.post(flask_api_url + "/api/projects",
+                                 data=data.encode('utf-8'),
+                                 headers={'Content-Type': 'application/json5'})
+        #projects_id = response.json()[0]
+        assert(response.json()== 33)
+        requests.post(
+            flask_api_url + '/api/projects/' + projects_id + '/comment',
+            data='new comment')
+        requests.get(flask_api_url + '/api/users/logout')
+        requests.post(flask_api_url + '/api/users/login',
+                      data=dict(email='user@knex.com', password="user"))
+        user_notifications = requests.get(flask_api_url + '/api/users/notifications')
+        assert (
+            user_notifications['link'] == '/project/' + projects_id)
