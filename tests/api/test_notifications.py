8279ffe74890924ad0cd446690930ee66309ec5e
aTepliak <a.tepliak@gmail.com>
2017-07-21 14:56:37 +0200

    updated test

--- tests/api/test_notifications.py
+++ tests/api/test_notifications.py
@@ -8,7 +8,7 @@ from uuid import UUID
 
 
 class Test_notifications(object):
-    def test_project_notification(self,session, flask_api_url, pytestconfig):
+    def test_project_notification(self, session, flask_api_url, pytestconfig):
         # login first
         requests.post(flask_api_url + '/api/users/login',
                       data=dict(email='admin@knex.com', password="admin"))
@@ -22,16 +22,17 @@ class Test_notifications(object):
         with open(test_manifest, 'r', encoding='utf-8') as tf:
             data = str(tf.read().replace('\n', ''))
         response = session.post(flask_api_url + "/api/projects",
-                                 data=data.encode('utf-8'),
-                                 headers={'Content-Type': 'application/json5'})
-        #projects_id = response.json()[0]
-        assert(response.json()== 33)
+                                data=data.encode('utf-8'),
+                                headers={'Content-Type': 'application/json5'})
+        projects_id = response.json()[0]
+        # assert(response.json()== 33)
         requests.post(
             flask_api_url + '/api/projects/' + projects_id + '/comment',
             data='new comment')
         requests.get(flask_api_url + '/api/users/logout')
         requests.post(flask_api_url + '/api/users/login',
                       data=dict(email='user@knex.com', password="user"))
-        user_notifications = requests.get(flask_api_url + '/api/users/notifications')
+        user_notifications = requests.get(
+            flask_api_url + '/api/users/notifications').json()
         assert (
             user_notifications['link'] == '/project/' + projects_id)
