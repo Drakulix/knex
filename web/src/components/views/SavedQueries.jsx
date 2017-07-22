f7d84cc96440d14494cdecf50497516ee160b907
Oliver Hanssen <oliver.hanssen@gmail.com>
2017-07-22 15:30:48 +0200

    saved search with real data

--- web/src/components/views/SavedQueries.jsx
+++ web/src/components/views/SavedQueries.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import SavedQuery from "../common/SavedQuery"
-
+import { get } from '../common/Backend'
 
 class Headline extends Component {
   render() {
@@ -23,60 +23,45 @@ export default class SavedQueries extends Component {
 
   componentWillMount(){
 
-//TODO LOAD
+    var queries = []
+      get("api/users/saved_searches").then(function(data) {
+
+        var datas = []
+
+        if(data !== undefined){
+          datas = data
+        }
+        for(let entry of datas){
 
 
-    var queries =  [
-      {
-        authors : [{email:"admin@knex.com", name :"Max Mustermann"}]
+          var content = [];
+          content["authors"] = entry["meta"]["authors"]
 
-                    ,
-        title : "Testd",
-        tags : ["DDSA"],
-        status : "DONE",
-        date_from :"2009-12-07",
-        date_to:"2015-05-06",
-        description : "ttt",
-        label : "TEST",
-        _id :"FDAF", //REMOVE
-        userID:"av" //REMOVE
-      },
-      {
-        label : "Empty",
-        searchString : "D",
-        _id :"Empty", //REMOVE
-        userID:"av" //REMOVE
-      },
-      {
-      label : "TEST",
-      tags : ["aa"],
-      title : "a",
-      authors : [{email:"admin@knex.com", name :"Max Mustermann"}],
-      description :"a",
-      status : "DONE",
-      date_to : "2014-06-12",
-      _id :"FDfAFs", //REMOVE
-      userID:"av" //REMOVE
-    },
-      {
-      authors : [{email:"marko@knex", name :"Marko"},
-                    {email:"marko@knex", name :"Marko"}
-                  ],
-      title : "Test",
-      tags : ["DDSA"],
-      status : "DONE",
-      date_from :"2009-12-07",
-      date_to:"2015-05-06",
-      description : "ttt",
-      searchString : "RE",
-      label : "TEST",
-      _id :"FDAFs", //REMOVE
-      userID:"av" //REMOVE
-    }];
+          content["_id"] = entry["id"]
+          content["tags"] = ["TAG"]
+          if(entry["meta"]["title"] !== undefined) content["title"] = entry["meta"]["title"];
+          if(entry["meta"]["value"] !== undefined) content["value"] = entry["meta"]["value"];
+          if(entry["meta"]["status"] !== undefined) content["status"] = entry["meta"]["status"];
+          if(entry["meta"]["filter_date_from"] !== undefined) content["filter_date_from"] = entry["meta"]["filter_date_from"];
+          if(entry["meta"]["filter_date_to"] !== undefined) content["filter_date_to"] = entry["meta"]["filter_date_to"];
+          if(entry["meta"]["description"] !== undefined) content["description"] = entry["meta"]["description"];
+          if(entry["meta"]["searchString"] !== undefined) content["searchString"] = entry["meta"]["searchString"];
+          if(entry["meta"]["shortName"] !== undefined) content["shortName"] = entry["meta"]["shortName"];
+          if(entry["meta"]["userID"] !== undefined) content["userID"] = entry["meta"]["userID"];
 
-    this.setState({
-    queries : queries
+       queries.push(
+    content
+
+       )
+      }
+
+
+        var d = queries
+        this.setState({
+        queries : queries
     })
+        console.log(this.state.queries)
+    }.bind(this));
   }
 
   render() {
