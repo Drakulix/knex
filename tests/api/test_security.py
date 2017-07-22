90485a7f987cfa36332cdbff32cef151e072a7ef
aTepliak <a.tepliak@gmail.com>
2017-07-22 21:47:33 +0200

    session reinitialization

--- tests/api/test_security.py
+++ tests/api/test_security.py
@@ -12,34 +12,34 @@ class TestPOST(object):
         response = session.get(flask_api_url + '/')
         assert response.status_code == 200
 
-    def test_login_fake_user(self,session, flask_api_url):
+    def test_login_fake_user(self, session, flask_api_url):
         session = requests.Session()
         response = session.post(flask_api_url + '/api/users/login',
-                                 data=dict(email='user1', password='password'))
+                                data=dict(email='user1', password='password'))
 
         assert response.reason == 'FORBIDDEN'
         assert response.status_code == 403
 
-    def test_login_real_user(self,session, flask_api_url):
+    def test_login_real_user(self, session, flask_api_url):
         response = session.post(flask_api_url + '/api/users/login',
-                                 data=dict(email='admin@knex.com', password="admin"))
+                                data=dict(email='admin@knex.com', password="admin"))
         assert response.reason == 'OK'
         assert response.status_code == 200
 
-    def test_login_real_user_wrong_psswd(self,session, flask_api_url):
+    def test_login_real_user_wrong_psswd(self, session, flask_api_url):
         response = session.post(flask_api_url + '/api/users/login',
-                                 data=dict(email='admin', password='a'))
+                                data=dict(email='admin', password='a'))
         assert response.reason == 'FORBIDDEN'
         assert response.status_code == 403
 
-    def test_logout(self, flask_api_url,session):
+    def test_logout(self, flask_api_url, session):
         response = session.get(flask_api_url + '/api/users/login',
-                                data=dict(email='user@knex.com', password="user"))
+                               data=dict(email='user@knex.com', password="user"))
         response = session.get(flask_api_url + '/api/users/logout')
         assert response.reason == 'OK'
         assert response.status_code == 200
 
-    def test_access_login_required_logged(self,session, pytestconfig, flask_api_url):
+    def test_access_login_required_logged(self, session, pytestconfig, flask_api_url):
         test_manifest = os.path.join(
             str(pytestconfig.rootdir),
             'tests',
@@ -49,8 +49,8 @@ class TestPOST(object):
         with open(test_manifest, 'r', encoding='utf-8') as tf:
             data = str(tf.read().replace('\n', ''))
         response = session.post(flask_api_url + "/api/projects",
-                                 data=data.encode('utf-8'),
-                                 headers={'Content-Type': 'application/json5'})
+                                data=data.encode('utf-8'),
+                                headers={'Content-Type': 'application/json5'})
 
         assert response.status_code == 403
 
@@ -65,18 +65,18 @@ class TestPOST(object):
         with open(test_manifest, 'r', encoding='utf-8') as tf:
             data = str(tf.read().replace('\n', ''))
         response = session.post(flask_api_url + "/api/projects",
-                                 data=data.encode('utf-8'),
-                                 headers={'Content-Type': 'application/json5'})
+                                data=data.encode('utf-8'),
+                                headers={'Content-Type': 'application/json5'})
 
         assert response.status_code == 403  # or 500?
 
-    def test_get_user(self, session,  flask_api_url):
+    def test_get_user(self, session, flask_api_url):
         response = session.get(flask_api_url + '/api/users/',
-                                data=dict(email='user@knex.com'))
+                               data=dict(email='user@knex.com'))
         assert response.status_code == 404  # 200?
 
-    def test_update_user_nonexistent(self,session, flask_api_url):
+    def test_update_user_nonexistent(self, session, flask_api_url):
         response = session.get(flask_api_url + '/api/users/',
-                                data=dict(email='unknownuser@knex.com'))
+                               data=dict(email='unknownuser@knex.com'))
         assert response.status_code == 404
-
+        session = requests.Session()
