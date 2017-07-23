c7f17211de146bf6b8f1aaee62130a44d95afc06
aTepliak <a.tepliak@gmail.com>
2017-07-23 14:18:29 +0200

    cast link to string

--- backend/api/helper/uploader.py
+++ backend/api/helper/uploader.py
@@ -42,7 +42,8 @@ def save_manifest_to_db(manifest):
                     list(set(
                         [author['email'] for author in entry['authors']] +
                         g.users_with_bookmark(str(entry['_id'])))),
-                    "Project was updated", entry['title'], entry['_id'])
+                    "Project was updated", entry['title'],
+                    '/project/' + str(entry['_id']))
                 g.rerun_saved_searches()
             return ids
         else:
@@ -54,9 +55,11 @@ def save_manifest_to_db(manifest):
                 validation_error["errors"].append(error.message)
                 for suberror in sorted(error.context, key=lambda e: e.schema_path):
                     validation_error["sub_errors"].append(suberror.message)
-            raise ApiException("Validation Error: \n" + str(is_valid), 400, validation_error)
+            raise ApiException("Validation Error: \n" + str(is_valid), 400,
+                               validation_error)
 
     except ApiException as e:
         raise e
     except Exception as err:
-        raise ApiException("Error while trying to save the document(s) into db." + str(err))
+        raise ApiException(
+            "Error while trying to save the document(s) into db." + str(err))
