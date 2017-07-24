8225d53f112a145f637f879ee79f470f9aff3048
DubbleClick <marc@henderkes.com>
2017-07-24 22:41:12 +0200

    plain/text to text/plain

--- tests/api/test_notifications.py
+++ tests/api/test_notifications.py
@@ -9,8 +9,7 @@ class TestPOST(object):
         sessionB.post(flask_api_url + '/api/users/login',
                       data=dict(email='user@knex.com', password="user"))
         project_id = enter_data_using_post.json()[0]
-        res = sessionB.post(flask_api_url + '/api/projects/' + project_id + '/comment',
-                            data="new comment", headers={'Content-type': "plain/text"})
-        assert res.status_code == 200
+        sessionB.post(flask_api_url + '/api/projects/' + project_id + '/comment',
+                      data="new comment", headers={'Content-type': "text/plain"})
         notifications = session.get(flask_api_url + '/api/users/notifications').json()
         assert notifications[-1]['link'] == '/project/' + project_id
