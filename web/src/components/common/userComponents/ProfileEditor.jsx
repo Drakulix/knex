338e1005574d209b49365b31d99ae3970a03a5b6
harasic <harasic@inf.fu-berlin.de>
2017-08-03 21:40:35 +0200

    Nicer refresh

--- web/src/components/common/userComponents/ProfileEditor.jsx
+++ web/src/components/common/userComponents/ProfileEditor.jsx
@@ -145,7 +145,7 @@ export default class ProfileEditor extends Component {
           </form>
         </div>
         <div className = "col-3">
-          <img  src = {`/api/users/${this.props.profileInf.email}/avatar?${new Date().getTime()}`}
+          <img  src = {`/api/users/${this.props.profileInf.email}/avatar?${Date.now()}`}
                 width = "200px"
                 height = "200px"
                 alt = "..."
