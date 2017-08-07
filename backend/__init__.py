1677f0af3fb21d021470697ea0f0b50cac225df4
harasic <harasic@inf.fu-berlin.de>
2017-08-07 15:13:11 +0200

    Notification Rework

--- backend/__init__.py
+++ backend/__init__.py
@@ -277,7 +277,7 @@ def handle_insufficient_permission(error):
         a project or user. It delivers a 404 as users should not see endpoints,
         where they don't have permissions to.
     """
-    return make_response("Not found", 404)
+    return make_response("Not permitted", 403)
 
 
 @app.errorhandler(404)
